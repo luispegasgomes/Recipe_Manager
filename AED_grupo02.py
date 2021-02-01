b5011a1854c97159d8d836bd3d5e8306bd5c2441
João Carmo <40190116@esmad.ipp.pt>
2021-02-01 02:37:30 +0000

    commit

--- AED_grupo02.py
+++ AED_grupo02.py
@@ -1703,99 +1703,113 @@ def notificacoes():
     janela_not.mainloop()
 
 def catalogo():
+    
     def combobox_option(*args):
+        
+        option = ""
+
         if selected.get() == "Titulo":
 
-            f = open(receitas, "r", encoding="utf-8")
-            lista1 = f.readlines()
 
-            f.close()
+            f1 = open("ficheiros\\titulos.txt", "r", encoding="utf-8")
+            lista1 = f1.readlines()
+            f1.close()
 
             option = lista1
             cbx2.set(option[0])
+ 
         elif selected.get() == "Categoria":
 
-            f = open(categorias, "r", encoding="utf-8")
+            f = open("ficheiros\\categorias.txt", "r", encoding="utf-8")
             lista2 = f.readlines()
-
             f.close()
 
             option = lista2
             cbx2.set(option[0])
-        elif selected.get() == "Ingredientes":
-
-            f = open("ficheiros\\dados_ttk.txt", "r", encoding="utf-8")
-            lista3 = f.readlines()
-            f.close()
-            for linha in lista3:
-                campos = linha.split(";")
-                option = campos[4]
-
-            cbx2.set(option)
-
-
+        
         cbx2.config(values=option)
+
 
     def search():
 
         if selected.get() == "Procurar por...":
             messagebox.showerror("ERRO", "Não escolheu uma opção")
         else:
-            lbox.delete(0, "end")
+            linhas = tree.get_children()
+            for item in linhas:
+                tree.delete(item)
 
             f = open("ficheiros\\dados_ttk.txt", "r", encoding="utf-8")
+            
             lista = f.readlines()
             f.close()
-            for linha in lista:
-                print(linha)
-                campos = linha.split(";")
-                if cbx2.get() == campos[1] or cbx2.get() == campos[0] or campos[4]:
-                    lbox.insert("end", campos[0])
 
-    receitas = "ficheiros\\receitas.txt"
-    categorias = "ficheiros\\categorias.txt"
+            for linha in lista:
+                campos = linha.split(";")
+                if cbx2.get() in linha: #and utilizador == campos[0]:
+                    tree.insert("","end", values=(campos[1], campos[2], campos[3], campos[5], campos[4]))
+
+
+    #def sort():
+
+
+
+
 
     janela_catalogo = Tk()
-    janela_catalogo.geometry("900x500")
+    janela_catalogo.geometry("900x400")
     janela_catalogo.title("Catálogo de Receitas")
 
 
     # ---------LISTBOX----------
 
-    lbox = Listbox(janela_catalogo, height=20, width=50, bd=2)
-    lbox.place(x=40, y=50)
+    # Componente treeview 
+    tree = ttk.Treeview(janela_catalogo, columns = ("Nome da Receita", "Categoria", "Duração", "Descrição", "Ingredientes"), show = "headings")
+    tree.column("Nome da Receita", width = 100, anchor="c")
+    tree.column("Categoria", width = 100, anchor="c")
+    tree.column("Descrição", width = 100, anchor="c")
+    tree.column("Duração", width = 100, anchor="c")
+    tree.column("Ingredientes", width = 100, anchor="c")
+    tree.heading("Nome da Receita", text="Nome da Receita")
+    tree.heading("Categoria", text="Categoria")
+    tree.heading("Descrição", text="Descrição")
+    tree.heading("Duração", text="Duração")
+    tree.heading("Ingredientes", text="Ingredientes")
+    tree.place(x=15, y=50)
 
-    f = open(receitas, "r", encoding="utf-8")
+    f = open("ficheiros\\dados_ttk.txt", "r", encoding="utf-8")
     lista = f.readlines()
     f.close()
     for linha in lista:
-        lbox.insert("end", linha)
+        campos = linha.split(";")
+        #if (campos[0] == utilizador):
+        tree.insert("", "end", values = (campos[1], campos[2], campos[3], campos[5], campos[4]))
 
     # ------COMBOBOXES------
 
     search_lbl = Label(janela_catalogo, text="Procurar : ")
-    search_lbl.place(x=400, y=50)
+    search_lbl.place(x=540, y=50)
 
     selected = StringVar()
     selected.set("Procurar por...")
 
-    cbx1 = ttk.Combobox(janela_catalogo, width=15, textvariable=selected, values=["Titulo","Categoria","Ingredientes"])
+    cbx1 = ttk.Combobox(janela_catalogo, width=15, textvariable=selected, values=["Titulo","Categoria"])
     cbx1.bind("<<ComboboxSelected>>",combobox_option)
-    cbx1.place(x=590, y=50)
+    cbx1.place(x=730, y=50)
 
     cbx2 = ttk.Combobox(janela_catalogo, width=15)
-    cbx2.place(x=460, y=50)
+    cbx2.place(x=600, y=50)
 
     # ---------BUTTON---------
     search_btn = Button(janela_catalogo, text="Procurar", fg="white", width=9, height=2, relief='ridge', command = search, bg="#499dc0")
-    search_btn.place(x=400, y=90)
+    search_btn.place(x=540, y=90)
 
 
 
     # ------ORDENAR POR------
 
     sort_frame = LabelFrame(janela_catalogo, text="Ordenar por", bd=3, height=100, width=200)
-    sort_frame.place(x=400, y=250)
+    sort_frame.place(x=550, y=180)
 
     val = StringVar()
     val.set("views")
@@ -1806,9 +1820,16 @@ def catalogo():
     radio2.place(x=100, y=25)
 
     sort_btn = Button(janela_catalogo, text="Ordenar", width=10, height=2, fg="white", bg="#499dc0")
-    sort_btn.place(x=640, y=280)
+    sort_btn.place(x=780, y=200)
 
-
+    f = open("ficheiros\\dados_ttk.txt", "r", encoding="utf-8")
+    lista = f.readlines()
+    f.close()
+    for linha in lista:
+        campos = linha.split(";")
+        f1 = open("ficheiros\\titulos.txt", "a", encoding="utf-8")
+        f1.write(campos[1] + "\n")
+    f1.close()
 
     janela_catalogo.mainloop()
 
